a6246e5d1f5db48e3d5a25368d16843fcecc485d
kanghyeonLee <lkhlkh0823@handong.ac.kr>
2024-11-06 15:45:12 +0900

    new files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>22100483 LeeKangHyeon</h1>
+      <div style={{background:'yellow'}}>
+      <p style={{color:'blue'}}>hello</p>
+      <p style={{color:'red'}}>it's react project</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
